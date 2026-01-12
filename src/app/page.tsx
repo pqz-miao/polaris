ee0cc888fc731858ddc6b3c81399436307fbaf4a
pqz-miao <pangqingzhu2024@gmail.com>
2026-01-12 15:20:34 +0800

    03: database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { useQuery, useMutation } from "convex/react";
+
 import { Button } from "@/components/ui/button";
 
+import { api } from "../../convex/_generated/api";
+
 const Page = () => {
+  const projects = useQuery(api.projects.get);
+  const createProject = useMutation(api.projects.create);
+
   return (
-    <Button variant="destructive" className="border-8 border-b-blue-900">
-      Click me
-    </Button>
+    <div className="flex flex-col gap-2 p-4">
+      <Button onClick={() => createProject({ name: "New project123" })}>
+        Add new
+      </Button>
+      {projects?.map((project) => (
+        <div key={project._id} className="border rounded p-2 flex flex-col">
+          <p>{project.name}</p>
+          <p>Owner Id: {project.ownerId}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
